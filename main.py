50246e72114425b1abd2802e4e40a4a8e3247c1e
alexmacharia <alex.machariam@gmail.com>
2023-02-15 07:18:28 +0300

    Commit 1

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import numpy as np
-from keras.preprocessing import image
+from keras.utils import load_img, img_to_array
 from keras.applications import vgg16
 
 # Load keras vgg model that was pretrained on ImageNet database
 model = vgg16.VGG16()
 
-img = image.load("bay.jpg", target_size=(224,224))
+img = load_img("bay.jpg", target_size=(224,224))
 
 # Convert image to numpy array
-x = image.img_to_array(img)
+x = img_to_array(img)
 
 # Add fourth dimension
 x = np.expand_dims(x, axis=0)
 
 # Normalize the pixel values to between 0 and 1
 x = vgg16.preprocess_input(x)
+
+# Predict
+predictions = model.predict(x)
+
+
+# Look up the names of the predicted classes
+predicted_classes = vgg16.decode_predictions(predictions)
+
+print("Top predictions for this image:")
+
+for imagenet_id, name, likelihood in predicted_classes[0]:
+    print("Prediction: {} - {:2f}".format(name, likelihood))
